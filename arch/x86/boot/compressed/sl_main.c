aa7485a7c46ad1aa25a90897e80aa1ff165aa5ff
Piotr Król <piotr.krol@3mdeb.com>
2019-07-23 00:46:53 +0200

    arch/x86/boot/compressed: add debug prints over serial
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- arch/x86/boot/compressed/sl_main.c
+++ arch/x86/boot/compressed/sl_main.c
@@ -118,6 +118,33 @@ void sl_tpm_extend_pcr(struct tpm *tpm, u32 pcr, const u8 *data, u32 len)
 		sl_txt_reset(TXT_SLERROR_TPM_EXTEND);
 }
 
+void print_debug_chars_inl(void)
+{
+	asm volatile (  "pushq	%%rcx\n\t"
+			"pushq	%%rdx\n\t"
+			"pushq	%%rax\n\t"
+			"xorl	%%ecx, %%ecx\n\t"
+			"1:\n\t"
+			"cmpb	$5, %%cl\n\t"
+			"jz	2f\n\t"
+			"movw	$0x3f8, %%dx\n\t"
+			"addw	$5, %%dx\n\t"
+			"3:\n\t"
+			"inb	%%dx, %%al\n\t"
+			"testb	$0x20, %%al\n\t"
+			"jz	3b\n\t"
+			"movw	$0x3f8, %%dx\n\t"
+			"movb	$0x61, %%al\n\t"
+			"addb	%%cl, %%al\n\t"
+			"outb	%%al, %%dx\n\t"
+			"incb	%%cl\n\t"
+			"jmp	1b\n\t"
+			"2:\n\t"
+			"popq	%%rax\n\t"
+			"popq	%%rdx\n\t"
+			"popq	%%rcx\n\t"
+			: : : );
+}
 void sl_main(u8 *bootparams)
 {
 	struct tpm *tpm;
