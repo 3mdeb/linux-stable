4d0508426b57ca7107f01948ec225868fe4ecb94
Piotr Król <piotr.krol@3mdeb.com>
2019-07-23 00:47:46 +0200

    kernel/sched/idle: add more debug prints
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -345,8 +345,37 @@ void play_idle(unsigned long duration_ms)
 }
 EXPORT_SYMBOL_GPL(play_idle);
 
+void print_debug_chars_inl_2(void)
+{
+	asm volatile (  "pushq	%%rcx\n\t"
+			"pushq	%%rdx\n\t"
+			"pushq	%%rax\n\t"
+			"xorl	%%ecx, %%ecx\n\t"
+			"1:\n\t"
+			"cmpb	$5, %%cl\n\t"
+			"jz	2f\n\t"
+			"movw	$0x3f8, %%dx\n\t"
+			"addw	$5, %%dx\n\t"
+			"3:\n\t"
+			"inb	%%dx, %%al\n\t"
+			"testb	$0x20, %%al\n\t"
+			"jz	3b\n\t"
+			"movw	$0x3f8, %%dx\n\t"
+			"movb	$0x41, %%al\n\t"
+			"addb	%%cl, %%al\n\t"
+			"outb	%%al, %%dx\n\t"
+			"incb	%%cl\n\t"
+			"jmp	1b\n\t"
+			"2:\n\t"
+			"popq	%%rax\n\t"
+			"popq	%%rdx\n\t"
+			"popq	%%rcx\n\t"
+			: : : );
+}
+
 void cpu_startup_entry(enum cpuhp_state state)
 {
+	print_debug_chars_inl_2();
 	/*
 	 * This #ifdef needs to die, but it's too late in the cycle to
 	 * make this generic (ARM and SH have never invoked the canary
@@ -364,6 +393,7 @@ void cpu_startup_entry(enum cpuhp_state state)
 #endif
 	arch_cpu_idle_prepare();
 	cpuhp_online_idle(state);
+	print_debug_chars_inl_2();
 	while (1)
 		do_idle();
 }
