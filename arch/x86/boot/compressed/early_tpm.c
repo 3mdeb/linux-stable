5b3af7bb1221e6b7ccc213ffb57405841208d732
Michał Żygowski <michal.zygowski@3mdeb.com>
2019-10-26 11:40:24 +0200

    arch/x86/boot/compressed/early_tpm: fix TPM2 extend
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- arch/x86/boot/compressed/early_tpm.c
+++ arch/x86/boot/compressed/early_tpm.c
@@ -18,11 +18,59 @@
 
 static u8 locality = TPM_NO_LOCALITY;
 
-
-
-
 #define TPM_CRB_DATA_BUFFER_OFFSET	0x80
 #define TPM_CRB_DATA_BUFFER_SIZE	3966
+
+/*
+ * Durations derived from Table 15 of the PTP but is purely an artifact of this
+ * implementation.
+ */
+
+/* TPM Duration A: 20ms */
+static void duration_a(void)
+{
+	tpm_udelay(20000);
+}
+
+/* TPM Duration B: 750ms */
+static void duration_b(void)
+{
+	tpm_udelay(750000);
+}
+
+/* TPM Duration C: 1000ms */
+static void duration_c(void)
+{
+	tpm_udelay(1000000);
+}
+
+/* Timeouts defined in Table 16 of the PTP */
+
+/* TPM Timeout A: 750ms */
+static void timeout_a(void)
+{
+	tpm_udelay(750000);
+}
+
+/* TPM Timeout B: 2000ms */
+static void timeout_b(void)
+{
+	tpm_udelay(2000000);
+}
+
+/* TPM Timeout C: 200ms */
+static void timeout_c(void)
+{
+	tpm_udelay(200000);
+}
+
+/* TPM Timeout D: 30ms */
+static void timeout_d(void)
+{
+	tpm_udelay(30000);
+}
+
+
 
 u8 *tpmb_reserve(struct tpmbuff *b)
 {
@@ -270,7 +318,10 @@ size_t tis_send(struct tpmbuff *buf)
 	if (locality > TPM_MAX_LOCALITY)
 		return 0;
 
-	tpm_write8(STS_COMMAND_READY, STS(locality));
+	for (status = 0; (status & STS_COMMAND_READY) == 0; ) {
+		tpm_write8(STS_COMMAND_READY, STS(locality));
+		status = tpm_read8(STS(locality));
+	}
 
 	buf_ptr = buf->head;
 
@@ -337,8 +388,11 @@ size_t tis_recv(struct tpmbuff *buf)
 		goto err;
 
 	/* ensure that there is data available */
-	if (!tis_data_available(locality))
-		goto err;
+	if (!tis_data_available(locality)) {
+		timeout_d();
+		if (!tis_data_available(locality))
+			goto err;
+	}
 
 	/* read header */
 	hdr = (struct tpm_header *)buf->head;
@@ -471,55 +525,6 @@ struct tpm_crb_intf_id_ext {
 		};
 	};
 } __packed;
-
-/*
- * Durations derived from Table 15 of the PTP but is purely an artifact of this
- * implementation.
- */
-
-/* TPM Duration A: 20ms */
-static void duration_a(void)
-{
-	tpm_udelay(20);
-}
-
-/* TPM Duration B: 750ms */
-static void duration_b(void)
-{
-	tpm_udelay(750);
-}
-
-/* TPM Duration C: 1000ms */
-static void duration_c(void)
-{
-	tpm_udelay(1000);
-}
-
-/* Timeouts defined in Table 16 of the PTP */
-
-/* TPM Timeout A: 750ms */
-static void timeout_a(void)
-{
-	tpm_udelay(750);
-}
-
-/* TPM Timeout B: 2000ms */
-static void timeout_b(void)
-{
-	tpm_udelay(2000);
-}
-
-/* TPM Timeout C: 200ms */
-static void timeout_c(void)
-{
-	tpm_udelay(200);
-}
-
-/* TPM Timeout D: 30ms */
-static void timeout_d(void)
-{
-	tpm_udelay(30);
-}
 
 static u8 is_idle(void)
 {
@@ -819,7 +824,7 @@ static int tpm2_alloc_cmd(struct tpmbuff *b, struct tpm2_cmd *c, u16 tag,
 static u16 convert_digest_list(struct tpml_digest_values *digests)
 {
 	int i;
-	u16 size = 0;
+	u16 size = sizeof(digests->count);;
 	struct tpmt_ha *h = digests->digests;
 
 	for (i = 0; i < digests->count; i++) {
@@ -854,6 +859,8 @@ static u16 convert_digest_list(struct tpml_digest_values *digests)
 		}
 	}
 
+	digests->count = cpu_to_be32(digests->count);
+
 	return size;
 }
 
@@ -866,42 +873,47 @@ int tpm2_extend_pcr(struct tpm *t, u32 pcr,
 	u16 size;
 	int ret = 0;
 
+	b = alloc_tpmbuff(t->intf, locality);
+
 	ret = tpm2_alloc_cmd(b, &cmd, TPM_ST_SESSIONS, TPM_CC_PCR_EXTEND);
 	if (ret < 0)
 		return ret;
 
-	cmd.handles = (u32 *)tpmb_put(b, sizeof(u32));
+	cmd.handles = (u32 *)tpmb_put(b, 2*sizeof(u32));
 	if (cmd.handles == NULL) {
 		tpmb_free(b);
+		free_tpmbuff(b, t->intf);
 		return -ENOMEM;
 	}
 
-	*cmd.handles = cpu_to_be32(pcr);
+	cmd.handles[0] = cpu_to_be32(pcr);
 
-	cmd.auth = (struct tpm2b *)tpmb_put(b, tpm2_null_auth_size());
+	cmd.auth = tpmb_put(b, tpm2_null_auth_size());
 	if (cmd.auth == NULL) {
 		tpmb_free(b);
+		free_tpmbuff(b, t->intf);
 		return -ENOMEM;
 	}
 
-	cmd.auth->size = tpm2_null_auth(cmd.auth->buffer);
-	cmd.auth->size = cpu_to_be16(cmd.auth->size);
+	cmd.handles[1] = cpu_to_be32(tpm2_null_auth(cmd.auth));
 
 	size = convert_digest_list(digests);
 	if (size == 0) {
 		tpmb_free(b);
+		free_tpmbuff(b, t->intf);
 		return -EINVAL;
 	}
 
 	cmd.params = (u8 *)tpmb_put(b, size);
 	if (cmd.params == NULL) {
 		tpmb_free(b);
+		free_tpmbuff(b, t->intf);
 		return -ENOMEM;
 	}
 
 	memcpy(cmd.params, digests, size);
 
-	cmd.header->size = cpu_to_be16(tpmb_size(b));
+	cmd.header->size = cpu_to_be32(tpmb_size(b));
 
 	switch (t->intf) {
 	case TPM_DEVNODE:
@@ -919,6 +931,7 @@ int tpm2_extend_pcr(struct tpm *t, u32 pcr,
 	}
 
 	tpmb_free(b);
+	free_tpmbuff(b, t->intf);
 	return ret;
 }
 
@@ -1027,8 +1040,7 @@ int tpm_extend_pcr(struct tpm *t, u32 pcr, u16 algo,
 {
 	int ret = 0;
 
-	if (t->family == TPM12 ||
-	    (t->family == TPM20 && t->intf == TPM_TIS)) {
+	if (t->family == TPM12) {
 		struct tpm_digest d;
 
 		if (algo != TPM_ALG_SHA1) {
@@ -1073,7 +1085,7 @@ int tpm_extend_pcr(struct tpm *t, u32 pcr, u16 algo,
 			goto out;
 		}
 
-		ret = tpm2_extend_pcr(t, pcr, d);
+		ret = !tpm2_extend_pcr(t, pcr, d);
 	} else {
 		ret = -EINVAL;
 	}
